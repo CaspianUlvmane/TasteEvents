ad51ede8f8f2240caa5bc4491f10fe3c37cc7730
Caspian Ulvmåne <cabbeulv@gmail.com>
2024-06-13 12:31:35 +0200

    react cleanup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Car from "./Car";
+
 import Header from "./Header";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const main = ReactDOM.createRoot(document.getElementById("main"));
 const headerDom = ReactDOM.createRoot(document.querySelector("header"));
 
-function Garage() {
-  return (
-    <>
-      <h1>Who lives in my garage?</h1>
-      <Car name="Volvo" model="V70" />
-    </>
-  );
-}
-
 headerDom.render(<Header />);
-root.render(<Garage />);
