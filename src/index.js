94b2b75404fe3a60edd808c84848c3d85e25afb8
Caspian Ulvmåne <cabbeulv@gmail.com>
2024-06-13 12:32:21 +0200

    correct main root

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import Header from "./Header";
 
-const main = ReactDOM.createRoot(document.getElementById("main"));
+const main = ReactDOM.createRoot(document.querySelector("main"));
 const headerDom = ReactDOM.createRoot(document.querySelector("header"));
 
 headerDom.render(<Header />);
