3765b6484d8ee6e1105ffd91ec7530aef1e64b1a
Caspian Ulvmåne <cabbeulv@gmail.com>
2024-06-28 14:36:44 +0200

    Blog introduction and link

---
+++ src/components/blogTeaser.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "./Image";
+import "./blogTeaser.css";
+function BlogTeaser(obj) {
+  console.log(obj);
+  let object = obj.obj;
+  if (object.data.Images[0] === undefined) {
+    return;
+  } else
+    return (
+      <>
+        <div id={object.id} className="blogTeaser">
+          <div className="imageContainer">
+            <a href={"/blogPost/" + object.id}>
+              <Image url={object.data.Images[0]} alt={object.data.Title} />
+            </a>
+            <div className="imageDropShadow"></div>
+          </div>
+          <div className="blogTeaserText">
+            <h2>{object.data.Title}</h2>
+            <p>{object.data.TextContent[0]}</p>
+          </div>
+        </div>
+      </>
+    );
+}
+
+export default BlogTeaser;
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,26 @@
+import React from "react";
+import db from "../db/firebase";
+import { collection, doc, getDoc, getDocs, query } from "firebase/firestore";
+import BlogTeaser from "../components/blogTeaser";
+
+const keysRef = query(collection(db, "BlogPosts"));
+const postKeys = await getDocs(keysRef);
+
+let blogPosts = [];
+postKeys.forEach((doc) => {
+  console.log(doc.id, doc.data());
+  blogPosts.push({ id: doc.id, data: doc.data() });
+});
+
 function Blog() {
-  return <h1>Blog Page</h1>;
+  console.log(blogPosts[0]);
+  let post = blogPosts[0];
+  console.log(post.id);
+  return (
+    <>
+      <BlogTeaser obj={post} />;
+    </>
+  );
 }
 
 export default Blog;
